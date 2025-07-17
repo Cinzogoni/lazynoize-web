fb99726a3ffcba9193a62ae4aa29144cd91ef3c1
DESKTOP-VDMI65C\PC <contactwork.huytc@gmail.com>
2025-07-17 19:37:44 +0700

    Product-Tab-Building

--- src/pages/ProductsPage/ProductsPage.tsx
+++ src/pages/ProductsPage/ProductsPage.tsx
@@ -2,18 +2,32 @@ import styles from "./ProductsPage.module.scss";
 import classNames from "classnames/bind";
 const cx = classNames.bind(styles);
 
+import ReactPlayer from "react-player";
+
 import { pageNameClasses } from "../../layouts/Header/Header";
 
 function ProductsPage() {
   return (
     <div className={cx(pageNameClasses.pro)}>
-      <h1
-        style={{
-          fontSize: "3.2rem",
-        }}
-      >
-        Sản phẩm của chúng tôi
-      </h1>
+      <div className={cx("pro-team")}>
+        <h1 className={cx("title")}>Sản phẩm của Lazy Noize</h1>
+        <div className={cx("products")}></div>
+      </div>
+
+      <div className={cx("pro-cus")}>
+        <h1 className={cx("title")}>Sản phẩm sản xuất bởi Lazy Noize</h1>
+        <div className={cx("products")}></div>
+      </div>
+
+      <div className={cx("pro-team")}>
+        <h1 className={cx("title")}>Music Video sản xuất bởi Lazy Noize</h1>
+        <div className={cx("products")}></div>
+      </div>
+
+      <div className={cx("pro-cus")}>
+        <h1 className={cx("title")}>Sản phẩm thu vocal tại Lazy Noize</h1>
+        <div className={cx("products")}></div>
+      </div>
     </div>
   );
 }
