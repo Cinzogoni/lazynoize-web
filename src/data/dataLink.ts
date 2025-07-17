0fb8147e3db590a794962a5d3a3b5aced2e38cfc
DESKTOP-VDMI65C\PC <contactwork.huytc@gmail.com>
2025-07-17 21:11:21 +0700

    Product-Responsive-GridSystem_01

---
+++ src/data/dataLink.ts
@@ -0,0 +1,11 @@
+// Sản phẩm của Lazy Noize
+
+export const proURLs = [
+  { a1: "https://www.youtube.com/watch?v=zfH0gvjc4JA" },
+  { a2: "https://www.youtube.com/watch?v=nplJq6r6pIM" },
+  { a3: "https://www.youtube.com/watch?v=JIgpyfVeiYM" },
+  { a4: "https://www.youtube.com/watch?v=wPz22s1r6-8" },
+  { a5: "https://www.youtube.com/watch?v=o6meB8NL26o" },
+  { a6: "https://www.youtube.com/watch?v=Db99mOWpt9k" },
+  { a7: "https://www.youtube.com/watch?v=JzxW_yNH7pw" },
+];
--- src/pages/ProductsPage/ProductsPage.tsx
+++ src/pages/ProductsPage/ProductsPage.tsx
@@ -3,15 +3,38 @@ import classNames from "classnames/bind";
 const cx = classNames.bind(styles);
 
 import ReactPlayer from "react-player";
-
 import { pageNameClasses } from "../../layouts/Header/Header";
+import { proURLs } from "../../data/dataLink";
+
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
+import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
+import GridSystem from "../../styles/GridSystem";
 
 function ProductsPage() {
   return (
     <div className={cx(pageNameClasses.pro)}>
       <div className={cx("pro-team")}>
         <h1 className={cx("title")}>Sản phẩm của Lazy Noize</h1>
-        <div className={cx("products")}></div>
+
+        <div className={cx("pro-box")}>
+          <ArrowBackIosIcon style={{ width: "120px", height: "120px" }} />
+          <div className={cx("products")}>
+            {proURLs.map((url, index) => {
+              const videoLink = Object.values(url)[0];
+              return (
+                <div className={cx("pro-frame")} key={index}>
+                  <ReactPlayer
+                    url={videoLink}
+                    width="98%"
+                    height="98%"
+                    controls={true}
+                  />
+                </div>
+              );
+            })}
+          </div>
+          <ArrowForwardIosIcon style={{ width: "120px", height: "120px" }} />
+        </div>
       </div>
 
       <div className={cx("pro-cus")}>
